2ab46c1a786707418e7ccc1ae8503d432237c277
Đorđe Drozgović <120629097+Drozga@users.noreply.github.com>
2024-11-02 20:09:09 +0100

    Add files via upload

---
+++ Oscilatory_Circular_motion_MPU6050/Script.m
@@ -0,0 +1,173 @@
+clear;
+close all;
+clc;
+addpath('Quaternions');
+addpath('Podaci sa senzora');
+
+filePath = 'data.csv';
+data = readtable(filePath, 'VariableNamingRule', 'preserve');
+
+samplePeriod = 1/256;
+% Access data based on original column names
+gyr = [data.("Gyroscope X (deg/s)"), ...  % Gyroscope X data
+       data.("Gyroscope Y (deg/s)"), ...  % Gyroscope Y data
+       data.("Gyroscope Z (deg/s)")];      % Gyroscope Z data
+
+acc = [data.("Accelerometer X (g)"), ...  % Accelerometer X data
+       data.("Accelerometer Y (g)"), ...  % Accelerometer Y data
+       data.("Accelerometer Z (g)")];      % Accelerometer Z data
+% Plot
+figure('NumberTitle', 'off', 'Name', 'Gyroscope');
+hold on;
+plot(gyr(:,1), 'r');
+plot(gyr(:,2), 'g');
+plot(gyr(:,3), 'b');
+xlabel('sample');
+ylabel('dps');
+title('Gyroscope');
+legend('X', 'Y', 'Z');
+
+figure('NumberTitle', 'off', 'Name', 'Accelerometer');
+hold on;
+plot(acc(:,1), 'r');
+plot(acc(:,2), 'g');
+plot(acc(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('Accelerometer');
+legend('X', 'Y', 'Z');
+
+%% Process data through AHRS algorithm (calcualte orientation)
+% See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
+
+R = zeros(3,3,length(gyr));     % rotation matrix describing sensor relative to Earth
+
+ahrs = MahonyAHRS('SamplePeriod', samplePeriod, 'Kp', 1);
+
+for i = 1:length(gyr)
+    ahrs.UpdateIMU(gyr(i,:) * (pi/180), acc(i,:));	% gyroscope units must be radians
+    R(:,:,i) = quatern2rotMat(ahrs.Quaternion)';    % transpose because ahrs provides Earth relative to sensor
+end
+
+%% Calculate 'tilt-compensated' accelerometer
+
+tcAcc = zeros(size(acc));  % accelerometer in Earth frame
+
+for i = 1:length(acc)
+    tcAcc(i,:) = R(:,:,i) * acc(i,:)';
+end
+
+% Plot
+figure('NumberTitle', 'off', 'Name', '''Kompenzovan nagib'' accelerometer');
+hold on;
+plot(tcAcc(:,1), 'r');
+plot(tcAcc(:,2), 'g');
+plot(tcAcc(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('''Kompenzovan nagib'' accelerometer');
+legend('X', 'Y', 'Z');
+
+%% Calculate linear acceleration in Earth frame (subtracting gravity)
+
+linAcc = tcAcc - [zeros(length(tcAcc), 1), zeros(length(tcAcc), 1), ones(length(tcAcc), 1)];
+linAcc = linAcc * 9.81;     % convert from 'g' to m/s/s
+
+% Plot
+figure('NumberTitle', 'off', 'Name', 'Linearno ubrzanje');
+hold on;
+plot(linAcc(:,1), 'r');
+plot(linAcc(:,2), 'g');
+plot(linAcc(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('Linearno ubrzanje');
+legend('X', 'Y', 'Z');
+
+%% Calculate linear velocity (integrate acceleartion)
+
+linVel = zeros(size(linAcc));
+
+for i = 2:length(linAcc)
+    linVel(i,:) = linVel(i-1,:) + linAcc(i,:) * samplePeriod;
+end
+
+% Plot
+figure('NumberTitle', 'off', 'Name', 'Linearna brzina');
+hold on;
+plot(linVel(:,1), 'r');
+plot(linVel(:,2), 'g');
+plot(linVel(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('Linearna brzina');
+legend('X', 'Y', 'Z');
+
+%% High-pass filter linear velocity to remove drift
+
+order = 1;
+filtCutOff = 0.5;
+[b, a] = butter(order, (2*filtCutOff)/(1/samplePeriod), 'high');
+linVelHP = filtfilt(b, a, linVel);
+
+% Plot
+figure('NumberTitle', 'off', 'Name', 'Linearna brzina sa visokopropusnim filtriranjem');
+hold on;
+plot(linVelHP(:,1), 'r');
+plot(linVelHP(:,2), 'g');
+plot(linVelHP(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('Linearna brzina sa visokopropusnim filtriranjem');
+legend('X', 'Y', 'Z');
+
+%% Calculate linear position (integrate velocity)
+
+linPos = zeros(size(linVelHP));
+
+for i = 2:length(linVelHP)
+    linPos(i,:) = linPos(i-1,:) + linVelHP(i,:) * samplePeriod;
+end
+
+% Plot
+figure('NumberTitle', 'off', 'Name', 'Linearna pozicija');
+hold on;
+plot(linPos(:,1), 'r');
+plot(linPos(:,2), 'g');
+plot(linPos(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('Linearna pozicija');
+legend('X', 'Y', 'Z');
+
+%% High-pass filter linear position to remove drift
+
+order = 1;
+filtCutOff = 3;
+[b, a] = butter(order, (2*filtCutOff)/(1/samplePeriod), 'high');
+linPosHP = filtfilt(b, a, linPos);
+
+% Plot
+figure('NumberTitle', 'off', 'Name', 'Linearna pozicija sa visokopropusnim filtriranjem');
+hold on;
+plot(linPosHP(:,1), 'r');
+plot(linPosHP(:,2), 'g');
+plot(linPosHP(:,3), 'b');
+xlabel('sample');
+ylabel('g');
+title('Linearna pozicija sa visokopropusnim filtriranjem');
+legend('X', 'Y', 'Z');
+%% Convert linear position from meters to centimeters
+linPosHP_cm = linPosHP * 10000;  % Convert position data to centimeters
+%% Play animation in centimeters
+SamplePlotFreq = 0.8;
+SixDOFanimation(linPosHP_cm, R, ...
+                'SamplePlotFreq', SamplePlotFreq, ...
+                'Trail', 'DotsOnly', ... % Enable only the dot trail
+                'Position', [9 39 1280 720], ...
+                'AxisLength', 10, ...   % Adjust axis length for centimeters
+                'ShowArrowHead', false, ... % No orientation arrows
+                'Xlabel', 'Z (cm)', 'Ylabel', 'X (cm)', 'Zlabel', 'Y (cm)', ...  % Update labels to cm
+                'ShowLegend', false, 'Title', 'Kruzno kretanje', ...
+                'CreateAVI', false, 'AVIfileNameEnum', false, 'AVIfps', ((1/samplePeriod) / SamplePlotFreq));
+%% End of script
